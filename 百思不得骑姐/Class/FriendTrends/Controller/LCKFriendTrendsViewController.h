f9580a00bdd7b0649f0016774a5e0542656cae7d
黄海良 <DesignerOne@huanghailiangmacmini.local>
2016-05-04 18:58:21 +0800

    关注栏，使用xib来进行（因为数据都是死数据，没必要使用代码来控制）。 注意点： 1. option＋enter快捷键换行  。 2. file owner的绑定

--- 百思不得骑姐/Class/FriendTrends/Controller/LCKFriendTrendsViewController.h
+++ 百思不得骑姐/Class/FriendTrends/Controller/LCKFriendTrendsViewController.h
@@ -6,6 +6,11 @@
 //  Copyright © 2016年 黄海良. All rights reserved.
 //
 
+
+//file，s owner要将其设置为相应的控制器，并将其连线到view中，才能绑定（自定义xib的时候，若是和controller文件一起建立的xib则会自动的绑定上的）
+
+//在label控件中要想换行则可使用option＋enter（小知识点推荐）
+
 #import <UIKit/UIKit.h>
 
 @interface LCKFriendTrendsViewController : UIViewController
