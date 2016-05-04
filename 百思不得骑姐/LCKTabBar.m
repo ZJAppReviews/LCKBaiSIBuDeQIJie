059c08c85044b8f2b8129ce534f95f545b1bc6d9
黄海良 <DesignerOne@huanghailiangmacmini.local>
2016-05-04 11:56:10 +0800

    自定义TabBar，并搭建底部控制栏的骨架

---
+++ 百思不得骑姐/LCKTabBar.m
@@ -0,0 +1,54 @@
+//
+//  LCKTabBar.m
+//  百思不得骑姐
+//
+//  Created by 黄海良 on 16/5/4.
+//  Copyright © 2016年 黄海良. All rights reserved.
+//
+
+#import "LCKTabBar.h"
+@interface LCKTabBar()
+@property(nonatomic,weak) UIButton *publishButton;
+@end
+
+@implementation LCKTabBar
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        //自定义控件初始化，必须实现这个方法
+        UIButton *publishButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [publishButton setBackgroundImage:[UIImage imageNamed:@"tabBar_publish_icon"] forState:UIControlStateNormal];
+        [publishButton setBackgroundImage:[UIImage imageNamed:@"tabBar_publish_click_icon"] forState:UIControlStateHighlighted];
+        [self addSubview:publishButton];
+        
+        self.publishButton = publishButton;
+        
+    }
+    return self;
+}
+
+-(void)layoutSubviews{
+    [super layoutSubviews];
+    
+    //设置发布按钮的frame
+    self.publishButton.bounds = CGRectMake(0, 0, self.publishButton.currentBackgroundImage.size.width, self.publishButton.currentBackgroundImage.size.height);
+    self.publishButton.center = CGPointMake(self.frame.size.width * 0.5, self.frame.size.height * 0.5);
+    
+    //设置其它按钮的frame
+    CGFloat buttonW = self.frame.size.width / 5 ;
+    CGFloat buttonY = 0;
+    CGFloat buttonH = self.frame.size.height;
+    NSUInteger index = 0 ;
+    
+    for (UIView *button in self.subviews) {
+        if (![button isKindOfClass:NSClassFromString(@"UITabBarButton")])  continue; //背景和＋位置不变外，其它都可以发生改变
+        
+        CGFloat buttonX = buttonW * ((index > 1)?(index + 1): index);
+        button.frame = CGRectMake(buttonX, buttonY, buttonW, buttonH);
+        
+        index ++;//增加索引
+    }
+}
+
+@end
---
+++ 百思不得骑姐/LCKtabBarController.m
@@ -0,0 +1,77 @@
+//
+//  LCKtabBarController.m
+//  百思不得骑姐
+//
+//  Created by 黄海良 on 16/5/4.
+//  Copyright © 2016年 黄海良. All rights reserved.
+//
+
+#import "LCKtabBarController.h"
+#import "LCKEssenceViewController.h"
+#import "LCKNewViewController.h"
+#import "LCKFriendTrendsViewController.h"
+#import "LCKMeViewController.h"
+#import "LCKTabBar.h"
+
+@interface LCKtabBarController ()
+
+@end
+
+@implementation LCKtabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    //文字的属性
+    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
+    attrs[NSFontAttributeName] = [UIFont systemFontOfSize:12];
+    attrs[NSForegroundColorAttributeName] = [UIColor grayColor];
+    
+    NSMutableDictionary *selectAtrrs = [NSMutableDictionary dictionary];
+    selectAtrrs[NSFontAttributeName] = [UIFont systemFontOfSize:12];
+    selectAtrrs[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
+    
+    //凡事方法后面带有UI_APPEARANCE_SELECTOR的都可以通过设置其appearance来统一控制所有的外观，如下所示
+    UITabBarItem *item = [UITabBarItem appearance];
+    [item setTitleTextAttributes:attrs forState:UIControlStateNormal];
+    [item setTitleTextAttributes:selectAtrrs forState:UIControlStateSelected];
+    
+    //添加子控制器(自定义的控件)
+    [self initChildVC:[[LCKEssenceViewController alloc] init] title:@"精华" image:@"tabBar_essence_icon" selectImage:@"tabBar_essence_click_icon"];
+    
+    [self initChildVC:[[LCKNewViewController alloc] init] title:@"新帖" image:@"tabBar_new_icon" selectImage:@"tabBar_new_click_icon"];
+    
+//    [self initChildVC:[[UIViewController alloc] init] title:@"发布" image:@"tabBar_publish_icon" selectImage:@"tabBar_publish_click_icon"];
+    
+    [self initChildVC:[[LCKFriendTrendsViewController alloc] init] title:@"关注" image:@"tabBar_friendTrends_icon" selectImage:@"tabBar_friendTrends_click_icon"];
+    
+    [self initChildVC:[[LCKMeViewController alloc] init] title:@"我" image:@"tabBar_me_icon" selectImage:@"tabBar_me_click_icon"];
+
+    
+    
+    //在布局Tabbar的控件时因为拿不到字控件的对象，因此可以通过更换自定义的tabBar来实现，并通过layoutSubViews方法来进行布局  (由于tabBar时read－only的，要修改则可使用KVC来进行修改［KVC能直接访问成员变量的］)
+//    self.tabBar = [[LCKTabBar alloc] init];
+    [self setValue:[[LCKTabBar alloc] init] forKey:@"tabBar"];
+
+}
+
+
+
+//初始化子控制器
+-(void)initChildVC:(UIViewController *)vc title:(NSString *)title image:(NSString *)image selectImage:(NSString *)selectImage
+{
+//    UIViewController *vc = [[class alloc] init];//这样对未来创建的控制器的扩展性不哈奥，因为有些控制器并不是通过init来创建的。
+    
+    vc.tabBarItem.title = title;
+    vc.tabBarItem.image = [UIImage imageNamed:image];
+    
+    //    //防止图片被渲染,另外一种方法就时通过图片在控制面板上的属性来设置
+    //    UIImage *image = [UIImage imageNamed:@"tabBar_essence_click_icon"];
+    //    image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    vc.tabBarItem.selectedImage = [UIImage imageNamed:selectImage];
+    vc.view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(100)/100.0 green:arc4random_uniform(100)/100.0 blue:arc4random_uniform(100)/100.0 alpha:1.0];
+    [self addChildViewController:vc];
+}
+
+@end
