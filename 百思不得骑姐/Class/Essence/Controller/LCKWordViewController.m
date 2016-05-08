3d9662213eea69ec8bd13bbdc9d11428dd0a10d4
黄海良 <DesignerOne@huanghailiangmacmini.local>
2016-05-08 18:26:22 +0800

    加载数据到段子控制器View中

--- 百思不得骑姐/Class/Essence/Controller/LCKWordViewController.m
+++ 百思不得骑姐/Class/Essence/Controller/LCKWordViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "LCKWordViewController.h"
+#import "AFNetWorking.h"
+#import "SVProgressHUD.h"
+#import "UIImageView+WebCache.h"
 
 @interface LCKWordViewController ()
-
+/**
+ *  帖子到数目
+ */
+@property(nonatomic,strong) NSArray *topics;
 @end
 
 @implementation LCKWordViewController
@@ -17,23 +23,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"type"] = @"29";
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        //将数据写入到硬盘中
+//        [responseObject writeToFile:@"/Users/AppleDeveloper/Desktop/Word.plist" atomically:YES];
+        
+        self.topics = responseObject[@"list"];
+        
+        [self.tableView reloadData];
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [SVProgressHUD showErrorWithStatus:@"加载段子数据错误"];
+    }];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 50;
+    return self.topics.count;
 }
 
 
@@ -42,11 +56,14 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellID];
     
     if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellID];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellID];
     }
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%@---%zd",[self class],indexPath.row];
-    
+    NSDictionary *topic = self.topics[indexPath.row];
+    cell.textLabel.text = topic[@"name"];
+    cell.detailTextLabel.text = topic[@"text"];
+    NSString *url = topic[@"profile_image"];
+    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:url] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
     return cell;
 }
 @end
